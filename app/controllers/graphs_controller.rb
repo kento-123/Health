cacf74133ff368d3ee4893c3b2c98fb058d55309
EC2 Default User <ec2-user@ip-172-31-6-171.ap-northeast-1.compute.internal>
2021-03-19 02:34:32 +0000

    graph2 physicalpost

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -1,17 +1,29 @@
 class GraphsController < ApplicationController
     
 def index
-  #@physical = current_user.physicals
-  #@weights = @physicals.pluck(:weight) 
   
-  @physical = [
-    Physical.new(weight: 70, created_at: Time.zone.parse("2021/3/4")),
-    Physical.new(weight: 75, created_at: Time.zone.parse("2021/3/7")),
-    Physical.new(weight: 80, created_at: Time.zone.parse("2021/3/9")),
-    Physical.new(weight: 85, created_at: Time.zone.parse("2021/3/11")),
-    ]
+  @physical = current_user.physicals
+  
+      p "-----------------------------"
+      p @physical
+      p "------------------------------"
+      
+      
+  @weights = @physical.pluck(:weight) 
+  
+      p "-----------------------------"
+      p @weights
+      p "------------------------------"
+  
+  #@physical = [
+    #Physical.new(weight: 70, created_at: Time.zone.parse("2021/3/4")),
+    #Physical.new(weight: 75, created_at: Time.zone.parse("2021/3/7")),
+    #Physical.new(weight: 80, created_at: Time.zone.parse("2021/3/9")),
+    #Physical.new(weight: 85, created_at: Time.zone.parse("2021/3/11")),
+    #]
     
-  @weights = @physical.pluck(:weight)
+  #@weights = @physical.pluck(:weight)
+  #@weights  = @physical.weight
 
   end
 
--- app/controllers/physicals_controller.rb
+++ app/controllers/physicals_controller.rb
@@ -5,17 +5,26 @@ def new
 end
 
 def create
-    def create
-        physical_params = params.require(:physicals).permit(:before_weight, :weight, :fat_percentage, :muscle, :metabolism)
+        physical_params = params.require(:physical).permit(:before_weight, :weight, :fat_percentage, :muscle, :metabolism)
+        p "-----------------------------"
+        p physical_params
+        p params
+        p "------------------------------"
+        
         @physical = Physical.new(physical_params)
         @physical.user_id = current_user.id
+        
+        p "-----------------------------"
+        p @physical
+        p "------------------------------"
+        
         if @physical.save
+            flash[:notice] = "登録しました"
             redirect_to posts_path
         else
+            flash.now[:alert] = "登録に失敗しました。"
             render :new
         end
         
-        
-    end
 end
 end
